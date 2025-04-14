91496b5318b77d32771a36568443074ded311c8f
koushik9988 <koushikkalita99@gmail.com>
2025-04-14 22:26:19 +0530

    initial commit

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kaushik/Documents/Codes/PIC/ePIC2D_mcc
-# Build directory: /home/kaushik/Documents/Codes/PIC/ePIC2D_mcc/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[SolverPerformanceTest]=] "/home/kaushik/Documents/Codes/PIC/ePIC2D_mcc/build/test_solvers")
-set_tests_properties([=[SolverPerformanceTest]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/kaushik/Documents/Codes/PIC/ePIC2D_mcc/CMakeLists.txt;97;add_test;/home/kaushik/Documents/Codes/PIC/ePIC2D_mcc/CMakeLists.txt;0;")
